3cdf4ad0072fe1ea3878beaa48001b15861d550b
Ryan McNeilly <ryanmcneilly@pop-os.localdomain>
2023-06-27 09:24:55 +1000

    Fixed database to work as a db file

--- backend/api.py
+++ backend/api.py
@@ -5,7 +5,7 @@ class API:
     def __init__(self, db: Database) -> None:
         self.db = db
 
-    def parse_response(self, request_method, request_body, path) -> str:
+    def parse_response(self, request_method, request_body, path) -> str:    
         """
         Process the request and return the result.
         :param args: The arguments passed to the API.
@@ -13,4 +13,14 @@ class API:
         """
         path = path.split("/")
 
+        # path = [auth, ]
+        if path[0] == "auth" and request_method == "POST":
+            handle_login_request()
+
+
+
         return {"success": False, "message": "Undefined behaviour"}
+
+# Handles user trying to authenticate
+def handle_login_request():
+    pass
