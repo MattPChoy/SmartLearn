08b45d5642150c721d8d5afb6e89db8b1a8479c5
Kevin <hasakev@gmail.com>
2023-06-30 22:33:42 +0800

    Completed written parts of requirements-spec2, began work on spec2

--- backend/vidtotext.py
+++ backend/vidtotext.py
@@ -1,26 +1,10 @@
-import speech_recognition as sr
-from pydub import AudioSegment
-import os
+import openai
 
 # Load the video file
 video = AudioSegment.from_file("transcript_test_files/video/test.mp4", format="mp4")
 audio = video.set_channels(1).set_frame_rate(16000).set_sample_width(2)
-audio.export("transcript_test_files/audio/audio.wav", format="wav")
+audio.export("transcript_test_files/audio/audio.mp3", format="mp3")
 
-# Initialize recognizer class (for recognizing the speech)
-r = sr.Recognizer()
+audio_file = open("transcript_test_files/audio/audio.mp3", "rb")
+transcript = openai.Audio.transcribe("whisper-1", audio_file)
 
-# Open the audio file
-with sr.AudioFile("transcript_test_files/audio/audio.wav") as source:
-    audio_text = r.record(source)
-# Recognize the speech in the audio
-# Need to download pocketSphinx
-
-text = r.recognize_sphinx(audio_text, language='en-US')
-
-# Print the transcript
-file_name = "transcript_test_files/transcript/transcription.txt"
-
-with open(file_name, "w") as file:
-    # Write to the file
-    file.write(text)
