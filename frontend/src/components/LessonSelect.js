60f0a34214d30af78c4d4d6c62478eae49e23fef
harri37 <harrison.wills@outlook.com>
2023-07-03 09:32:51 +0800

    feat: admin oage now loads admin courses

--- frontend/src/components/LessonSelect.js
+++ frontend/src/components/LessonSelect.js
@@ -1,11 +1,41 @@
 import { ListGroup } from "react-bootstrap";
 import { useLocation, useNavigate } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { Spinner } from "react-bootstrap";
 
-function LessonSelect({ lessons, course, offering }) {
+function LessonSelect({ course, offering }) {
   const location = useLocation();
   const navigate = useNavigate();
 
-  return (
+  const [lessons, setLessons] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const res = await fetch(
+          `http://localhost:5000/api/getLessons?offering_id=${offering}`,
+          {
+            method: "GET",
+            headers: { "Content-Type": "application/json" },
+          }
+        );
+        const resObj = await res.json();
+        console.log(resObj);
+        if (resObj.success === true) {
+          setLessons(resObj.data);
+          setLoading(false);
+        }
+      } catch (e) {
+        console.log(e);
+      }
+    }
+    fetchData();
+  }, [offering]);
+
+  return loading ? (
+    <Spinner />
+  ) : (
     <ListGroup>
       {lessons.map((lesson) => (
         <ListGroup.Item
